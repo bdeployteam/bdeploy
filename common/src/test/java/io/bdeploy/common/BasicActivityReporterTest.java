7d8629f8c174718508fa653300b23fe2ad3a75ad
Markus Duft <markus.duft@ssi-schaefer.com>
2019-04-30 08:56:07 +0200

    Add basic activity test
    Request: MO
    Change-Id: Ia949e6d0fe51830ec8427bfd6c5a4620b67f57c2

---
+++ common/src/test/java/io/bdeploy/common/BasicActivityReporterTest.java
@@ -0,0 +1,51 @@
+package io.bdeploy.common;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import io.bdeploy.common.ActivityReporter.Activity;
+
+public class BasicActivityReporterTest {
+
+    private static ActivityReporter.Stream stream = new ActivityReporter.Stream(System.out);
+
+    @BeforeAll
+    static void initStream() {
+        stream.beginReporting();
+    }
+
+    @AfterAll
+    static void deinitStream() {
+        stream.stopReporting();
+    }
+
+    static Stream<ActivityReporter> basicTest() {
+        return Stream.of(stream, new ActivityReporter.Null());
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void basicTest(ActivityReporter reporter) throws Exception {
+        try (Activity indeterminate = reporter.start("Indeterminate")) {
+            indeterminate.worked(1);
+
+            try (Activity nested = reporter.start("Determinate", 2)) {
+                nested.worked(1);
+                nested.workAndCancelIfRequested(1);
+            }
+        }
+
+        AtomicLong v = new AtomicLong(0);
+        try (Activity supplied = reporter.start("Fully Supplied", () -> 2l, () -> v.get())) {
+            v.set(2);
+        }
+
+        // if we reach this, we're ok :)
+    }
+
+}
