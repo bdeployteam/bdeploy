492260c82d94ccda6883275edce9dffb7f832779
Markus Duft <markus.duft@ssi-schaefer.com>
2019-05-16 08:10:02 +0200

    Add file association script for linux launcher
    Request: DCS-373
    Change-Id: I159cb7331982823e5e786c6d761ea0f2a2c272f8

---
+++ launcher/src/linux64/dist/bin/file-assoc.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Creates a file association and the according desktop entry to the launcher
+
+APP_HOME="$(cd $(dirname $0); pwd)"
+LAUNCHER_PATH="${APP_HOME}/launcher"
+
+TMP_MIME_DESC="${TMPDIR:-/tmp}/bdeploy-bdeploy.xml"
+TMP_DESK_DESC="${TMPDIR:-/tmp}/bdeploy-launcher.desktop"
+
+cat > ${TMP_MIME_DESC} <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+    <mime-type type="application/x-bdeploy">
+        <comment>A BDeploy launcher descriptor</comment>
+        <glob pattern="*.bdeploy"/>
+    </mime-type>
+</mime-info>
+EOF
+
+cat > ${TMP_DESK_DESC} <<EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+MimeType=application/x-bdeploy
+Name=BDeploy Click & Start launcher
+Comment=Interpreter for .bdeploy files
+TryExec=${LAUNCHER_PATH}
+Exec=${LAUNCHER_PATH} %f
+Terminal=false
+EOF
+
+xdg-mime install "${TMP_MIME_DESC}"
+xdg-icon-resource install --context mimetypes --size 64 "${APP_HOME}/logo64.png" application-x-bdeploy
+xdg-icon-resource install --context mimetypes --size 128 "${APP_HOME}/logo128.png" application-x-bdeploy
+xdg-desktop-menu install "${TMP_DESK_DESC}"
+xdg-mime default bdeploy-launcher.desktop application/x-bdeploy
+
+rm "${TMP_MIME_DESC}"
+rm "${TMP_DESK_DESC}"
