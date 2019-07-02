d6261f5ac1f726c09c8c61df8a7fe5f28b84c7a4
Michael Heiss <michael.heiss@ssi-schaefer.com>
2019-07-02 09:20:47 +0200

    Launcher: Add environmental and system details to log
    Request: DCS-484
    Change-Id: I7b659acc1b27ba45652b67f9531b9e40700ed71c

---
+++ common/src/main/java/io/bdeploy/common/util/ProcessHelper.java
@@ -0,0 +1,44 @@
+package io.bdeploy.common.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ProcessHelper {
+
+    private ProcessHelper() {
+    }
+
+    /**
+     * Launches the given process as described by the given builder and waits for termination. The output that is written
+     * to the standard out as well as standard error is returned as string.
+     * <p>
+     * <b>Implementation note:</b> Only suitable for short running processes that print out some details and terminate within
+     * seconds.
+     * </p>
+     *
+     * @param builder the process to launch
+     * @return the output of the process
+     */
+    public static String launch(ProcessBuilder builder) {
+        try {
+            builder.redirectErrorStream(true);
+            return readOutput(builder.start());
+        } catch (IOException ioe) {
+            return null;
+        }
+    }
+
+    /** Reads the output of the given process and returns it as plain string */
+    private static String readOutput(Process process) throws IOException {
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                result.append(line).append("\n");
+            }
+        }
+        return result.toString();
+    }
+
+}
