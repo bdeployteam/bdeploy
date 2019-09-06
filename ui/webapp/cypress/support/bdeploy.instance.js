5c51e7797bd62982dc26d1d77ea551aab5b98936
Markus Duft <markus.duft@ssi-schaefer.com>
2019-09-06 12:27:19 +0200

    Polish and activate cypress.io tests for builds
    Request: MO
    Change-Id: I4c613b60a5c8b1c82a27c426f7f4f751a07ecd4c

--- ui/webapp/cypress/support/bdeploy.instance.js
+++ ui/webapp/cypress/support/bdeploy.instance.js
@@ -1,23 +1,27 @@
 Cypress.Commands.add('createInstance', function(name) {
   cy.visit('/');
-  cy.get('[cypress=group-Test]').first().click();
+  cy.get('[data-cy=group-Test]').first().click();
+
+  // wait until the progress spinner disappears
+  cy.get('mat-progress-spinner').should('not.exist')
 
   // attention: the button contains 'add' not '+' (mat-icon('add') = '+')
   cy.get('button').contains('add').click();
 
   cy.get('[placeholder="Name"]').type(name)
+  cy.get('[placeholder="Description"]').type('Test Instance for automated test')
 
   // angular drop down is something very different from a native HTML select/option
   cy.get('[placeholder="Purpose"]').click()
   cy.get('mat-option').contains('TEST').click()
 
-  cy.get('[placeholder="Description"]').type('Test Instance for automated test')
   cy.get('[placeholder="Product"]').click()
   cy.get('mat-option').contains('demo/product').click();
 
   cy.get('[placeholder="Version"]').click();
-  cy.get('mat-option').first().click();
+  cy.get('mat-option').contains('2.0.0').click();
 
+  // finally the target, which is the configured backend with the configured token.
   cy.get('[placeholder="Master URL"]').type(Cypress.env('backendBaseUrl'))
 
   cy.get('body').then($body => {
@@ -25,7 +29,8 @@ Cypress.Commands.add('createInstance', function(name) {
       cy.get('mat-option').contains('localhost').click();
     } else {
       cy.fixture('token.json').then(fixture => {
-        cy.get('[placeholder="Security Token"]').type(fixture.token, { delay: 0 })
+        // don't use .type(fixture.token) as this mimiks a typing user (delay)
+        cy.get('[placeholder="Security Token"]').invoke('val', fixture.token).trigger('input')
       })
     }
   })
