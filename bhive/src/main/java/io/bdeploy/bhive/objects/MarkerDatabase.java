443a4efc866f926be416813b7be7ff4bce4bb03f
Markus Duft <markus.duft@ssi-schaefer.com>
2019-10-09 10:17:03 +0200

    Further locking to prevent PruneOperation from missing marker databases
    There was still a tiny whole where an operation could have deleted a
    marker database while the PruneOperation was reading the list of objects
    from the actual ObjectDatabase. In this case it would miss the manifest
    (not there yet), read all objects, and fail to protect them (marker
    database deleted).
    
    Request: DCS-691
    Change-Id: I5e9865387925e3a7b13d1be3a829e693630dbf69

--- bhive/src/main/java/io/bdeploy/bhive/objects/MarkerDatabase.java
+++ bhive/src/main/java/io/bdeploy/bhive/objects/MarkerDatabase.java
@@ -2,6 +2,7 @@ package io.bdeploy.bhive.objects;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -11,8 +12,6 @@ import io.bdeploy.common.util.PathHelper;
 
 public class MarkerDatabase extends ObjectDatabase {
 
-    private static final byte[] DEFAULT_MARKER = new byte[] { 0x1 };
-
     public MarkerDatabase(Path root, ActivityReporter reporter) {
         super(root, root.resolve("tmp"), reporter);
     }
@@ -21,7 +20,7 @@ public class MarkerDatabase extends ObjectDatabase {
         Path markerFile = getObjectFile(id);
         PathHelper.mkdirs(markerFile.getParent());
         try {
-            Files.write(markerFile, DEFAULT_MARKER);
+            Files.createFile(markerFile);
         } catch (IOException e) {
             throw new IllegalStateException("Cannot add marker for " + id, e);
         }
@@ -44,6 +43,64 @@ public class MarkerDatabase extends ObjectDatabase {
     @Override
     public void removeObject(ObjectId id) {
         throw new UnsupportedOperationException("Marker-only Database");
+    }
+
+    /**
+     * Lock a directory. The lock can be awaited using {@link #waitRootLock(Path)}.
+     * <p>
+     * The method waits for an already existing lock to disappear before proceeding (max 1000 seconds, ~16 minutes). This means
+     * only a single lock can exist (intra- & inter-VM).
+     *
+     * @param root the root directory to lock.
+     */
+    public static void lockRoot(Path root) {
+        for (int i = 0; i < 10_000; ++i) {
+            try {
+                Files.createFile(root.resolve(".lock"));
+                return;
+            } catch (FileAlreadyExistsException e) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            } catch (Exception e) {
+                throw new IllegalStateException("Cannot lock root", e);
+            }
+        }
+
+        throw new IllegalStateException("Retries exceeded or interrupted, failed to lock marker root");
+    }
+
+    /**
+     * Wait for a root lock to disappear (max 1000 seconds, ~16 minutes).
+     *
+     * @param root the root to wait for
+     */
+    public static void waitRootLock(Path root) {
+        Path lockFile = root.resolve(".lock");
+        for (int i = 0; i < 10_000; ++i) {
+            if (Files.exists(lockFile)) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            } else {
+                return;
+            }
+        }
+
+        throw new IllegalStateException("Retries exceeded or interrupted, failed to wait for marker root lock");
+    }
+
+    /**
+     * @param root root to unlock
+     */
+    public static void unlockRoot(Path root) {
+        PathHelper.deleteRecursive(root.resolve(".lock"));
     }
 
 }
