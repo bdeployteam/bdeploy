08cfc479f7c8dbedd594d43e2f4f3f274cc7d122
Markus Duft <markus.duft@ssi-schaefer.com>
2019-12-03 15:24:33 +0100

    Enable soft migration of existing persisted ManagedMasterDtos
    Request: MO
    Change-Id: Ieeca94ceb1dfca6454c7057c5365e200ff6a6db4

--- ui/src/main/java/io/bdeploy/ui/dto/ManagedMasterDto.java
+++ ui/src/main/java/io/bdeploy/ui/dto/ManagedMasterDto.java
@@ -2,10 +2,13 @@ package io.bdeploy.ui.dto;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import io.bdeploy.interfaces.minion.MinionConfiguration;
 
 public class ManagedMasterDto {
 
+    @JsonAlias("name") // old name
     public String hostName;
     public String description;
     public String uri;
