1303e72cfe7ddd8495cbae3f5f9349285801f942
Tobias Haubenwallner <tobias.haubenwallner@ssi-schaefer.com>
2020-08-10 11:41:15 +0200

    Fixed page-back button
    The only use for the routing-history service is to check if the user
    came from another site. It doesn't need to safe the pages.
    
    Change-Id: Ib81915ac9068a1d301944e4f8c02ad72c2529789

--- ui/webapp/src/app/modules/core/services/routing-history.service.ts
+++ ui/webapp/src/app/modules/core/services/routing-history.service.ts
@@ -1,21 +1,36 @@
+import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
-import { Router, RoutesRecognized } from '@angular/router';
+import { NavigationStart, Router, RoutesRecognized } from '@angular/router';
 import { filter, pairwise } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoutingHistoryService {
-  previousUrls:string[] = [];
-  i:RoutesRecognized;
-  constructor(private router:Router) {
-    router.events.pipe(filter(e => e instanceof RoutesRecognized),pairwise()).subscribe((e:any) => this.previousUrls.push(e.urlAfterRedirects));
+  private offset = 0;
+  private routedBack = false;
+
+  constructor(private router:Router, private location:Location) {
+    router.events.pipe(filter(e => e instanceof NavigationStart)).subscribe((e:NavigationStart) => {
+      if(e.navigationTrigger === "popstate"){
+        this.routedBack = true;
+        this.offset--;
+      }
+    });
+    router.events.pipe(filter(e => e instanceof RoutesRecognized),pairwise()).subscribe(() => {
+      if(!this.routedBack){
+        this.offset++;
+      }
+      this.routedBack = false;
+    });
   }
+
   back(defaultUrl:string){
-    if(this.previousUrls.length > 0){
-      this.router.navigateByUrl(this.previousUrls.pop())
+    if(this.offset>0){
+      this.location.back();
     }
     else{
+      this.routedBack = true;
       this.router.navigateByUrl(defaultUrl);
     }
   }
