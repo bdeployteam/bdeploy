c070bedc46db25359ebac3d7bcbcb0f045d847ce
Thomas Urlaub <thomas.urlaub@ssi-schaefer.com>
2020-11-18 16:39:13 +0100

    Refactoring of error interceptor
    Request: MO
    Change-Id: I7936fda045f3e0ed35c7f788d868a805c95ed7e5

--- ui/webapp/src/app/modules/core/interceptors/error-handler.interceptor.ts
+++ ui/webapp/src/app/modules/core/interceptors/error-handler.interceptor.ts
@@ -4,7 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Observable, of, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { NO_ERROR_HANDLING_HDR } from '../../../models/consts';
+import { NO_ERROR_HANDLING_HDR } from 'src/app/models/consts';
 import { ErrorMessage, Logger, LoggingService } from '../services/logging.service';
 import { SystemService } from '../services/system.service';
 
@@ -22,34 +22,35 @@ export class HttpErrorHandlerInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((e) => {
-        if (
-          e instanceof HttpErrorResponse &&
-          (e.status === 403 || (e.status === 404 && !request.headers.has(NO_ERROR_HANDLING_HDR)))
-        ) {
-          this.snackbar.open(
-            `Unfortunately, /${e.url} was not found (wrong URL or insufficient rights), we returned you to the safe-zone.`,
-            'DISMISS',
-            { panelClass: 'error-snackbar' }
-          );
-          this.router.navigate(['/instancegroup/browser']);
-          return of(null);
-        } else if (e instanceof HttpErrorResponse && e.status === 499) {
-          // special version mismatch code.
-          this.log.errorWithGuiMessage(new ErrorMessage(e.statusText, e));
-        } else if (e instanceof HttpErrorResponse && e.status !== 401 && !request.headers.has(NO_ERROR_HANDLING_HDR)) {
-          // let 401 pass through for logout redirection in the other interceptor :)
-          if (e.status === 0) {
-            this.systemService.backendUnreachable();
-          } else {
-            let displayPath = request.url;
-            try {
-              displayPath = new URL(request.url).pathname;
-            } catch (error) {
-              this.log.warn(new ErrorMessage('Cannot parse request URL', error));
-            }
-            this.log.errorWithGuiMessage(new ErrorMessage(e.status + ': ' + e.statusText + ': ' + displayPath, e));
+        if (e instanceof HttpErrorResponse && !request.headers.has(NO_ERROR_HANDLING_HDR)) {
+          switch (e.status) {
+            case 0:
+              this.systemService.backendUnreachable();
+              return of(null);
+            case 401:
+              // let 401 pass through for logout redirection in the other interceptor :)
+              break;
+            case 403:
+              // no break
+            case 404:
+              const msg = `Unfortunately, /${e.url} was not found (wrong URL or insufficient rights), we returned you to the safe-zone.`;
+              this.snackbar.open(msg, 'DISMISS', { panelClass: 'error-snackbar' });
+              this.router.navigate(['/instancegroup/browser']);
+              return of(null);
+            case 499:
+              // special version mismatch code.
+              this.log.errorWithGuiMessage(new ErrorMessage(e.statusText, e));
+              break;
+            default:
+              let displayPath = request.url;
+              try {
+                displayPath = new URL(request.url).pathname;
+              } catch (error) {
+                this.log.warn(new ErrorMessage('Cannot parse request URL', error));
+              }
+              this.log.errorWithGuiMessage(new ErrorMessage(e.status + ': ' + e.statusText + ': ' + displayPath, e));
+              return of(null);
           }
-          return of(null);
         }
         return throwError(e);
       })
