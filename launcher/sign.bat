54e0eb5aa5a1da53fcbe80ecf1aea7e0fc29125a
Michael Heiss <michael.heiss@ssi-schaefer.com>
2021-07-19 10:42:21 +0200

    Code-Signing: Update tools and ReadMe
    *) Signing is now only done when creating a Release build. This is done
    as the actual signing with a real hardware token is rather slow. No need
    to do that for Debug builds.
    
    *) Signing can be always done manually by using the sign.bat tool that
    is located in the launcher.
    
    Change-Id: Iefdac244990dbb106ac0b7e969b58d93e5bd4528

--- launcher/sign.bat
+++ launcher/sign.bat
@@ -1,8 +1,14 @@
 @echo off
 
-rem This only works if the code signing certificate is installed on this windows machine.
+rem This script will sign all BDeploy executables
+rem This only works if the code signing tools are installed on this windows machine.
 rem The EV certificate requires a hardware token in addition to the certificate.
-rem Currently only Markus Duft's machine has the token and the certificate. It will only work there.
+
+rem Prerequisites: 
+rem Connect the hardware token
+rem Download Windows 10 SDK:  https://developer.microsoft.com/en-US/windows/downloads/windows-10-sdk/
+rem Download SafeNet Drivers: https://support.globalsign.com/ssl/ssl-certificates-installation/safenet-drivers#Windows
+rem Add the following directory to the local path C:\Program Files (x86)\Windows Kits\10\bin\<version>\x64
 
 signtool sign /n "IT Solutions GmbH" /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 src\win64\dist\BDeploy.exe
 signtool sign /n "IT Solutions GmbH" /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 src\win64\dist\FileAssoc.exe
