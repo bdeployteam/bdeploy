61acee2134bf4a559377870f92268ec71c88bf79
Markus Duft <markus.duft@ssi-schaefer.com>
2021-07-23 10:06:16 +0200

    Permissions: show all users in a single table instead of add button
    Request: DCS-1325
    Change-Id: If254c039ca55ff3ca22e1bc0d3eed29d95a32786

--- ui/webapp/src/app/modules/panels/groups/components/settings/permissions/permissions.component.ts
+++ ui/webapp/src/app/modules/panels/groups/components/settings/permissions/permissions.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { BehaviorSubject, combineLatest, Subscription } from 'rxjs';
 import { BdDataColumn, BdDataGrouping } from 'src/app/models/data';
 import { Permission, UserInfo } from 'src/app/models/gen.dtos';
 import { ACTION_CANCEL, ACTION_OK, BdDialogMessageAction } from 'src/app/modules/core/components/bd-dialog-message/bd-dialog-message.component';
@@ -8,7 +7,6 @@ import { GroupsService } from 'src/app/modules/primary/groups/services/groups.se
 import { UsersColumnsService } from '../../../services/users-columns.service';
 import { UsersService } from '../../../services/users.service';
 import { PermissionLevelComponent } from './permission-level/permission-level.component';
-import { UserSelectOptionComponent } from './user-select-option/user-select-option.component';
 
 @Component({
   selector: 'app-permissions',
@@ -37,7 +35,7 @@ export class PermissionsComponent implements OnInit, OnDestroy {
     name: 'Modify',
     data: (r) => `Modify permissions for ${r.name}`,
     action: (r) => this.doModify(this.modDialog, r),
-    icon: (r) => 'edit',
+    icon: (r) => (!this.getLocalPermissionLevel(r) && !this.getGlobalPermissionLevel(r) ? 'add' : 'edit'),
     width: '40px',
   };
 
@@ -48,44 +46,31 @@ export class PermissionsComponent implements OnInit, OnDestroy {
     disabled: () => !this.getLocalPermissionLevel(this.modUser),
   };
 
-  /* template */ records$ = new BehaviorSubject<UserInfo[]>(null);
-  /* template */ available$ = new BehaviorSubject<UserInfo[]>(null);
+  private groupNames = ['Local Permission', 'Global Permission', 'No Permission'];
+
   /* template */ columns: BdDataColumn<UserInfo>[] = [...this.userCols.defaultUsersColumns, this.colGlobalPerm, this.colLocalPerm, this.colModPerm];
   /* template */ grouping: BdDataGrouping<UserInfo>[] = [
     {
       definition: {
-        group: (r) => (!this.getLocalPermissionLevel(r) ? 'Global Permission' : 'Local Permission'),
+        group: (r) => (!this.getLocalPermissionLevel(r) ? (!this.getGlobalPermissionLevel(r) ? this.groupNames[2] : this.groupNames[1]) : this.groupNames[0]),
         name: 'Permission Type',
-        sort: (a, b) => b.localeCompare(a),
+        sort: (a, b) => this.groupNames.indexOf(a) - this.groupNames.indexOf(b),
       },
       selected: [],
     },
   ];
 
-  /* template */ readonly optionComponent = UserSelectOptionComponent;
   /* template */ modPerm: Permission;
   /* template */ modUser: UserInfo;
 
   @ViewChild(BdDialogComponent) private dialog: BdDialogComponent;
   @ViewChild('modDialog') private modDialog: TemplateRef<any>;
-  private subscription: Subscription;
 
   constructor(public groups: GroupsService, public users: UsersService, public userCols: UsersColumnsService) {}
 
-  ngOnInit(): void {
-    this.subscription = combineLatest([this.groups.current$, this.users.users$]).subscribe(([g, u]) => {
-      if (!g || !u) {
-        return;
-      }
+  ngOnInit(): void {}
 
-      this.records$.next(u.filter((u) => !!u.permissions.find((p) => p.scope === g.name || !p.scope)));
-      this.available$.next(u.filter((u) => !u.permissions.find((p) => p.scope === g.name || !p.scope)));
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+  ngOnDestroy(): void {}
 
   private getLocalPermissionLevel(user: UserInfo): Permission {
     return user.permissions.find((p) => p.scope === this.groups.current$.value.name)?.permission;
@@ -107,17 +92,6 @@ export class PermissionsComponent implements OnInit, OnDestroy {
           this.users.updatePermission(user, this.groups.current$.value.name, this.modPerm).subscribe();
         }
       });
-  }
-
-  /* template */ doAdd(tpl: TemplateRef<any>) {
-    this.modUser = null;
-    this.modPerm = null;
-    this.dialog.message({ header: 'Add User', template: tpl, actions: [ACTION_CANCEL, ACTION_OK] }).subscribe((r) => {
-      if (!r) {
-        return;
-      }
-      this.users.updatePermission(this.modUser, this.groups.current$.value.name, this.modPerm).subscribe();
-    });
   }
 
   /* template */ getAvailablePermissionsFor(user: UserInfo): Permission[] {
--- ui/webapp/src/app/modules/panels/groups/groups.module.ts
+++ ui/webapp/src/app/modules/panels/groups/groups.module.ts
@@ -2,19 +2,28 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CoreModule } from '../../core/core.module';
 import { AddGroupComponent } from './components/add-group/add-group.component';
-import { GroupsRoutingModule } from './groups-routing.module';
-import { SettingsComponent } from './components/settings/settings.component';
-import { EditComponent } from './components/settings/edit/edit.component';
-import { AttributeValuesComponent } from './components/settings/attribute-values/attribute-values.component';
-import { AttributeDefinitionsComponent } from './components/settings/attribute-definitions/attribute-definitions.component';
-import { MaintenanceComponent } from './components/settings/maintenance/maintenance.component';
 import { ClientDetailComponent } from './components/client-detail/client-detail.component';
-import { PermissionsComponent } from './components/settings/permissions/permissions.component';
+import { AttributeDefinitionsComponent } from './components/settings/attribute-definitions/attribute-definitions.component';
+import { AttributeValuesComponent } from './components/settings/attribute-values/attribute-values.component';
+import { EditComponent } from './components/settings/edit/edit.component';
+import { MaintenanceComponent } from './components/settings/maintenance/maintenance.component';
 import { PermissionLevelComponent } from './components/settings/permissions/permission-level/permission-level.component';
-import { UserSelectOptionComponent } from './components/settings/permissions/user-select-option/user-select-option.component';
+import { PermissionsComponent } from './components/settings/permissions/permissions.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { GroupsRoutingModule } from './groups-routing.module';
 
 @NgModule({
-  declarations: [AddGroupComponent, SettingsComponent, EditComponent, AttributeValuesComponent, AttributeDefinitionsComponent, MaintenanceComponent, ClientDetailComponent, PermissionsComponent, PermissionLevelComponent, UserSelectOptionComponent],
+  declarations: [
+    AddGroupComponent,
+    SettingsComponent,
+    EditComponent,
+    AttributeValuesComponent,
+    AttributeDefinitionsComponent,
+    MaintenanceComponent,
+    ClientDetailComponent,
+    PermissionsComponent,
+    PermissionLevelComponent,
+  ],
   imports: [CommonModule, CoreModule, GroupsRoutingModule],
 })
 export class GroupsModule {}
