726961bd861c1e7b326abdc539d02981ec8620a5
Markus Duft <markus.duft@ssi-schaefer.com>
2021-09-17 14:24:28 +0200

    Admin UI Tests: metrics and logging
    Request: DCS-1334
    Change-Id: I7cd468956e8d0467f9881ce2942836d49be79689

---
+++ ui/webapp/cypress/integration/admin-logging.spec.js
@@ -0,0 +1,37 @@
+describe('Admin UI Tests (Logging)', () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+  it('Tests Logging', () => {
+    cy.visit('/');
+    cy.get('.local-hamburger-button').click();
+    cy.get('button[data-cy=Administration]').click();
+
+    cy.contains('a', 'Logging').click();
+
+    cy.intercept({ method: 'GET', url: '/api/logging-admin/config' }).as('config');
+    cy.intercept({ method: 'POST', url: '**/api/logging-admin/content/master**' }).as('log');
+
+    cy.inMainNavContent(() => {
+      cy.get('tr').should('have.length.above', 1); // two audit logs which should always exist.
+
+      cy.pressToolbarButton('Edit Configuration');
+    });
+
+    cy.inMainNavFlyin('app-log-config-editor', () => {
+      cy.get('button[data-cy^="APPLY"]').should('exist').and('be.disabled');
+      cy.wait('@config');
+      cy.pressToolbarButton('Close');
+    });
+
+    cy.inMainNavContent(() => {
+      cy.contains('tr', 'audit.log').click();
+    });
+
+    cy.inMainNavFlyin('app-log-file-viewer', () => {
+      cy.wait('@log');
+      cy.pressToolbarButton('Close');
+    });
+  });
+});
---
+++ ui/webapp/cypress/integration/admin-metrics.spec.js
@@ -0,0 +1,22 @@
+describe('Admin UI Tests (Metrics)', () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+  it('Tests Metrics', () => {
+    cy.visit('/');
+    cy.get('.local-hamburger-button').click();
+    cy.get('button[data-cy=Administration]').click();
+
+    cy.contains('a', 'Metrics').click();
+
+    cy.inMainNavContent(() => {
+      cy.fillFormSelect(undefined, 'HTTP');
+      cy.fillFormSelect(undefined, 'HIVE');
+
+      cy.intercept({ method: 'GET', url: '/api/server-monitor' }).as('server');
+      cy.fillFormSelect(undefined, 'SERVER');
+      cy.wait('@server');
+    });
+  });
+});
