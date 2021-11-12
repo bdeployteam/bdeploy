89f48aeeec7eadc6e4f88474039d0e2db1951de7
Markus Duft <markus.duft@ssi-schaefer.com>
2021-11-12 10:03:33 +0100

    Add manual performance test for marker database with(-out) locking
    Bug: DCS-1429
    Change-Id: I9207294cd7f371c37696625b367b1ec2541967cf

--- bhive/src/test/java/io/bdeploy/bhive/misc/MarkerConcurrency.java
+++ bhive/src/test/java/io/bdeploy/bhive/misc/MarkerConcurrency.java
@@ -9,6 +9,8 @@ import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.bdeploy.bhive.BHive;
 import io.bdeploy.bhive.BHiveTransactions.Transaction;
@@ -18,6 +20,32 @@ import io.bdeploy.bhive.op.ImportOperation;
 
 @ExtendWith(TestHive.class)
 public class MarkerConcurrency {
+
+    private static final Logger log = LoggerFactory.getLogger(MarkerConcurrency.class);
+
+    @Test
+    public void testSequentialMarks(BHive hive, @TempDir Path tmp) throws Exception {
+
+        // a test for manual comparison of performance when removing locking, e.g. as added with ce525e07 in MarkerDatabase
+
+        Path source = tmp.resolve("source");
+        Files.createDirectories(source);
+
+        for (int i = 0; i < 1000; ++i) {
+            Path x = source.resolve("file" + i);
+            Files.write(x, Collections.singletonList("This is the content " + i));
+        }
+
+        long start = System.currentTimeMillis();
+        assertDoesNotThrow(() -> {
+            hive.setParallelism(1);
+            try (Transaction t = hive.getTransactions().begin()) {
+                hive.execute(new ImportOperation().setSourcePath(source).setManifest(new Manifest.Key("test", "1.0")));
+            }
+        });
+
+        log.info("Importing took {}ms", (System.currentTimeMillis() - start));
+    }
 
     @Test
     public void testConcurrentMarks(BHive hive, @TempDir Path tmp) throws Exception {
