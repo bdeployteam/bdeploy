685cd1e0ca926b539e4881ec2fad957c01318342
Dastan Yessekeyev <Dastan.Yessekeyev.extern@ssi-schaefer.com>
2022-05-23 12:35:06 +0600

    replacing shareReplay with share
    since shareReplay will become deprecated in the future
    
    Request: WPL-586
    Change-Id: I5a6674b8327a0bd3feb33895203de53835c5521a

--- ui/webapp/src/app/modules/core/services/http-replay.service.ts
+++ ui/webapp/src/app/modules/core/services/http-replay.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, shareReplay } from 'rxjs';
+import { Observable, ReplaySubject, share } from 'rxjs';
 
 interface ReplayCache {
   [someUrl: string]: Observable<any>;
@@ -16,7 +16,14 @@ export class HttpReplayService {
 
   public get<T>(url: string, resetTimeMs = 1000): Observable<T> {
     if (!this.cache[url]) {
-      this.cache[url] = this.http.get<T>(url).pipe(shareReplay(1));
+      this.cache[url] = this.http.get<T>(url).pipe(
+        share({
+          connector: () => new ReplaySubject(1),
+          resetOnError: true,
+          resetOnComplete: false,
+          resetOnRefCountZero: false,
+        })
+      );
       setTimeout(() => delete this.cache[url], resetTimeMs);
     }
     return this.cache[url];
