3b76770d3dcd5d14b8126146de772d71098d201e
Dastan Yessekeyev <Dastan.Yessekeyev.extern@ssi-schaefer.com>
2023-04-04 15:06:06 +0600

    Delete Product from SW Repo with local admin permissions leads to error
    Request: WPL-1595
    Change-Id: I07147bb193627890d4a3b6ff69b3a48eed263259

--- ui/src/main/java/io/bdeploy/ui/api/SoftwareRepositoryResource.java
+++ ui/src/main/java/io/bdeploy/ui/api/SoftwareRepositoryResource.java
@@ -3,6 +3,12 @@ package io.bdeploy.ui.api;
 import java.util.List;
 import java.util.SortedSet;
 
+import io.bdeploy.common.security.RequiredPermission;
+import io.bdeploy.common.security.ScopedPermission.Permission;
+import io.bdeploy.interfaces.UserInfo;
+import io.bdeploy.interfaces.UserPermissionUpdateDto;
+import io.bdeploy.interfaces.configuration.instance.SoftwareRepositoryConfiguration;
+import io.bdeploy.jersey.ActivityScope;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
@@ -12,13 +18,6 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
-import io.bdeploy.common.security.RequiredPermission;
-import io.bdeploy.common.security.ScopedPermission.Permission;
-import io.bdeploy.interfaces.UserInfo;
-import io.bdeploy.interfaces.UserPermissionUpdateDto;
-import io.bdeploy.interfaces.configuration.instance.SoftwareRepositoryConfiguration;
-import io.bdeploy.jersey.ActivityScope;
 
 @Path("/softwarerepository")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -50,6 +49,7 @@ public interface SoftwareRepositoryResource {
     public SoftwareResource getSoftwareResource(@ActivityScope @PathParam("softwareRepository") String softwareRepository);
 
     @Path("/{softwareRepository}/product")
+    @RequiredPermission(permission = Permission.CLIENT, scope = "softwareRepository")
     public ProductResource getProductResource(@ActivityScope @PathParam("softwareRepository") String softwareRepository);
 
     @GET
