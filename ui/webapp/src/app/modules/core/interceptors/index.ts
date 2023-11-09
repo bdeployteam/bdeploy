928cc40bf46a08f0eb105e8b212409cec01472ba
Dastan Yessekeyev <Dastan.Yessekeyev.extern@ssi-schaefer.com>
2023-11-09 18:30:18 +0600

    Prevent leaving site
    Request: WPL-2140
    Change-Id: Ida9b08255b31058b277949ec0d187f32f1819197

--- ui/webapp/src/app/modules/core/interceptors/index.ts
+++ ui/webapp/src/app/modules/core/interceptors/index.ts
@@ -1,6 +1,7 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { DelayForAuthInterceptor } from './delay-for-auth.interceptor';
 import { HttpErrorHandlerInterceptor } from './error-handler.interceptor';
+import { NoLoadingBarInterceptor } from './no-loading-bar.interceptor';
 import { UnauthorizedInterceptor } from './unauthorized.interceptor';
 
 export const httpInterceptorProviders = [
@@ -17,6 +18,11 @@ export const httpInterceptorProviders = [
   {
     provide: HTTP_INTERCEPTORS,
     useClass: HttpErrorHandlerInterceptor,
+    multi: true,
+  },
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: NoLoadingBarInterceptor,
     multi: true,
   },
 ];
