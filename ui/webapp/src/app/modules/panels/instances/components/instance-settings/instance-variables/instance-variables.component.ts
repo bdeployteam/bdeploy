094d91b5513e721b9efc2224e5514ea30361a936
Dastan Yessekeyev <Dastan.Yessekeyev.extern@ssi-schaefer.com>
2024-02-21 00:13:57 +0600

    add copy/paste for instance variables
    Request: WPL-1404
    Change-Id: If935fdebb9f2f750bd3c024a47d9fc4e69d51437

--- ui/webapp/src/app/modules/panels/instances/components/instance-settings/instance-variables/instance-variables.component.ts
+++ ui/webapp/src/app/modules/panels/instances/components/instance-settings/instance-variables/instance-variables.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit, TemplateRef, ViewChild, inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { cloneDeep } from 'lodash-es';
-import { Observable, Subscription, combineLatest, of, tap } from 'rxjs';
+import { Observable, Subscription, combineLatest, interval, of, startWith, tap } from 'rxjs';
 import { BdDataColumn } from 'src/app/models/data';
 import {
   ApplicationDto,
@@ -58,6 +59,7 @@ export class InstanceVariablesComponent implements DirtyableDialog, OnInit, OnDe
   private systems = inject(SystemsService);
   private areas = inject(NavAreasService);
   private plugins = inject(PluginService);
+  private snackbar = inject(MatSnackBar);
   protected edit = inject(InstanceEditService);
   protected groups = inject(GroupsService);
 
@@ -85,6 +87,8 @@ export class InstanceVariablesComponent implements DirtyableDialog, OnInit, OnDe
 
   protected records: ConfigVariable[] = [];
   protected columns: BdDataColumn<ConfigVariable>[] = [colName, colValue, colDesc, this.colEdit, this.colDelete];
+  protected checked: ConfigVariable[];
+  protected clipboardVars: ConfigVariable[];
 
   protected newValue: VariableConfiguration;
   protected newUsedIds: string[] = [];
@@ -135,6 +139,12 @@ export class InstanceVariablesComponent implements DirtyableDialog, OnInit, OnDe
     });
 
     this.subscription.add(this.areas.registerDirtyable(this, 'panel'));
+
+    this.subscription.add(
+      interval(1000)
+        .pipe(startWith(null))
+        .subscribe(() => this.readFromClipboard()),
+    );
   }
 
   ngOnDestroy(): void {
@@ -165,7 +175,93 @@ export class InstanceVariablesComponent implements DirtyableDialog, OnInit, OnDe
     return of(true).pipe(
       tap(() => {
         this.edit.conceal('Change Instance Variables');
-      })
+      }),
+    );
+  }
+
+  protected doCopy() {
+    const json = JSON.stringify(this.checked, null, '\t');
+
+    navigator.clipboard.writeText(json).then(
+      () =>
+        this.snackbar.open('Copied to clipboard successfully', null, {
+          duration: 1000,
+        }),
+      () =>
+        this.snackbar.open('Unable to write to clipboard.', null, {
+          duration: 1000,
+        }),
+    );
+  }
+
+  protected doPaste() {
+    if (!this.clipboardVars?.length) {
+      this.snackbar.open('Unable to read from clipboard.', null, {
+        duration: 1000,
+      });
+      return;
+    }
+    const newVars: VariableConfiguration[] = [];
+    const existingVars: VariableConfiguration[] = [];
+    const instance = this.edit.state$.value.config.config;
+    if (!instance.instanceVariables) {
+      instance.instanceVariables = [];
+    }
+    this.clipboardVars.forEach((instanceVar) => {
+      const found = instance.instanceVariables.some((iv) => iv.id === instanceVar.value.id);
+      if (found) {
+        existingVars.push(instanceVar.value);
+      } else {
+        newVars.push(instanceVar.value);
+      }
+    });
+    let message = `${this.clipboardVars.length} instance variables copied from clipboard. `;
+    if (newVars.length) {
+      instance.instanceVariables.push(...newVars);
+      message += `Added ${newVars.length} instance variables. `;
+    } else {
+      message += 'No new instance variables to add. ';
+    }
+
+    if (existingVars.length) {
+      message += `Skipped ${existingVars.length} instance variables for conflicting with existing ones.`;
+    }
+
+    instance.instanceVariables.sort((a, b) => a.id.localeCompare(b.id));
+    this.buildVariables(this.edit.state$.value.config);
+
+    this.snackbar.open(message, 'DISMISS');
+  }
+
+  private readFromClipboard() {
+    if (!navigator.clipboard.readText) {
+      // must be firefox. firefox allows reading the clipboard *only* from browser
+      // extensions but never from web pages itself. it is rumored that there is a config
+      // which can be enabled ("Dom.Events.Testing.AsynClipBoard"), however that did not
+      // change browser behaviour in tests.
+      this.clipboardVars = null;
+      console.error('Clipboard access is not supported in this browser. Pasting applications is not possible.');
+      return;
+    }
+    navigator.clipboard.readText().then(
+      (data) => {
+        this.clipboardVars = null;
+        try {
+          const instanceVariables: ConfigVariable[] = JSON.parse(data);
+          const validNames = instanceVariables.every((iv) => !!iv.name);
+          const validVariables = instanceVariables.every((iv) => !!iv.value && !!iv.value.id);
+          if (!validNames || !validVariables) {
+            console.error(`Invalid instance variables format.`);
+          }
+          this.clipboardVars = instanceVariables;
+        } catch (e) {
+          console.error('Unable to parse from clipboard', e);
+        }
+      },
+      (e) => {
+        console.error('Unable to read from clipboard', e);
+        this.clipboardVars = null;
+      },
     );
   }
 
@@ -250,7 +346,7 @@ export class InstanceVariablesComponent implements DirtyableDialog, OnInit, OnDe
     const vars = this.edit.state$.value.config.config.instanceVariables;
     vars.splice(
       vars.findIndex((x) => x.id === r.name),
-      1
+      1,
     );
     this.buildVariables(this.edit.state$.value.config);
   }
