7cefb1d9db68c002d4dd698b2372cc306f9c3eaa
Markus Neubauer <markus.neubauer@ssi-schaefer.com>
2024-05-07 11:02:15 +0200

    add sbom generator for webpack
    Request: none
    Change-Id: I498940f74eac6e1fcae13280d3ea4bd23abac1b3

---
+++ ui/webapp/webpack-build.config.js
@@ -0,0 +1,12 @@
+const { CycloneDxWebpackPlugin } = require('@cyclonedx/webpack-plugin')
+
+module.exports = {
+  plugins: [
+    new CycloneDxWebpackPlugin({
+      specVersion: '1.6',
+      outputLocation: '../../../../build/repots/webapp',
+      reproducibleResults: true,
+      validateResults: true
+    })
+  ]
+}
