8b53e17997fc8ca6b46e184617ddb53cb4ef8fc8
dkargl <daniel.kargl@ssi-schaefer.com>
2024-06-04 10:25:20 +0200

    Added constants for the colors of the browser table cell renderers
    Also made the logic in the purpose cell renderer more robust (and
    possibly a little overengineered).
    
    Request: None
    Change-Id: I2a3206e603b25a4a67f75a28194888da2b966cb8

---
+++ launcher/src/main/java/io/bdeploy/launcher/cli/ui/browser/BrowserDialogTableColorConstants.java
@@ -0,0 +1,18 @@
+package io.bdeploy.launcher.cli.ui.browser;
+
+import java.awt.Color;
+
+/**
+ * Contains constants for {@link Color colors} which are used by the browser dialog table.
+ */
+class BrowserDialogTableColorConstants {
+
+    static final Color DISABLED = Color.LIGHT_GRAY;
+    static final Color ENABLED = Color.GREEN;
+    static final Color PAY_ATTENTION = new Color(255, 255, 140);
+    static final Color COULD_NOT_CALCULATE = Color.RED;
+
+    static final Color PURPOSE_DEVELOPMENT = new Color(51, 170, 0);
+    static final Color PURPOSE_TEST = new Color(51, 170, 255);
+    static final Color PURPOSE_PRODUCTIVE = Color.WHITE;
+}
