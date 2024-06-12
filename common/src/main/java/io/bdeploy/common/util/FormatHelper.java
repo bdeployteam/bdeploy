a4a9ea6ba3f13aac22215cf8273ead748800808a
dkargl <daniel.kargl@ssi-schaefer.com>
2024-06-12 11:33:10 +0200

    Made FormatHelper#formatDuration thread save
    Turns out that SimpleDateFormat keeps an internal state which makes
    unsuitable for multi-threaded environments.
    
    Request: None
    Change-Id: I8903c8132dfaa50cd7d06dcbd4bf34e576375ee5

--- common/src/main/java/io/bdeploy/common/util/FormatHelper.java
+++ common/src/main/java/io/bdeploy/common/util/FormatHelper.java
@@ -16,7 +16,6 @@ import java.util.Locale;
  */
 public class FormatHelper {
 
-    private static final SimpleDateFormat DURATION_FORMAT = new SimpleDateFormat("mm 'min' ss 'sec' SSS 'ms'");
     private static final DecimalFormat SIZE_FORMAT = new DecimalFormat("#,##0.#");
     private static final DecimalFormat TRANSFER_FORMAT = new DecimalFormat("#0.0");
     private static final String[] SIZE_UNITS = new String[] { "B", "kB", "MB", "GB", "TB" };
@@ -30,7 +29,7 @@ public class FormatHelper {
      * Formats the given duration into a human readable format
      */
     public static String formatDuration(long timeInMillis) {
-        return DURATION_FORMAT.format(new Date(timeInMillis));
+        return new SimpleDateFormat("mm 'min' ss 'sec' SSS 'ms'").format(new Date(timeInMillis));
     }
 
     /**
