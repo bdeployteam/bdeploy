db309e3f86ad1bbb3bce0522b66421097434330d
Markus Duft <markus.duft@ssi-schaefer.com>
2024-07-09 09:08:19 +0200

    Suppress output from updating bat file on windows
    Bug: CT_BDEPLOY-176
    Change-Id: Idac1b5a0507f11769f38034e1b1f43ee8b7c5164

--- minion/src/win64/dist/bin/bdeploy.bat
+++ minion/src/win64/dist/bin/bdeploy.bat
@@ -2,8 +2,7 @@
 set SCRIPT_HOME=%~dp0
 
 if exist "%SCRIPT_HOME%_bdeploy-new.bat" (
-    echo "Updating start script..."
-    move /y "%SCRIPT_HOME%_bdeploy-new.bat" "%SCRIPT_HOME%_bdeploy.bat"
+    move /y "%SCRIPT_HOME%_bdeploy-new.bat" "%SCRIPT_HOME%_bdeploy.bat" >nul
 )
 
 "%SCRIPT_HOME%_bdeploy.bat" %*
