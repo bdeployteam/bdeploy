e3b30e25ec1d854fddb81cf95f3d85f29500f831
dkargl <daniel.kargl@ssi-schaefer.com>
2024-07-10 18:50:11 +0200

    Reduced scope of warning suppressions where possible
    Request: None
    Change-Id: I6b102542730e3a60cb65f5a62cc1e17443878fcf

--- bhive/src/main/java/io/bdeploy/bhive/op/remote/RemoteOperation.java
+++ bhive/src/main/java/io/bdeploy/bhive/op/remote/RemoteOperation.java
@@ -1,13 +1,13 @@
 package io.bdeploy.bhive.op.remote;
 
-import io.bdeploy.bhive.BHive;
+import io.bdeploy.bhive.BHive.Operation;
 import io.bdeploy.common.security.RemoteService;
 
-@SuppressWarnings("unchecked")
-public abstract class RemoteOperation<T, X extends RemoteOperation<T, X>> extends BHive.Operation<T> {
+public abstract class RemoteOperation<T, X extends RemoteOperation<T, X>> extends Operation<T> {
 
     private RemoteService remote;
 
+    @SuppressWarnings("unchecked")
     public X setRemote(RemoteService def) {
         this.remote = def;
         return (X) this;
@@ -16,5 +16,4 @@ public abstract class RemoteOperation<T, X extends RemoteOperation<T, X>> extend
     protected RemoteService getRemote() {
         return remote;
     }
-
 }
