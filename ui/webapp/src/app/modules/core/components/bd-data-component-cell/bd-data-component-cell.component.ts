5d3733fb78ad15d9fc33c4cecbf79e1128e43415
Dastan Yessekeyev <Dastan.Yessekeyev.extern@ssi-schaefer.com>
2024-08-09 14:44:24 +0500

    client app stale name after update fix
    Request: CT_BDEPLOY-161
    Change-Id: If3157ccd52d7fea33bd643f3b52f550abfb7b007

--- ui/webapp/src/app/modules/core/components/bd-data-component-cell/bd-data-component-cell.component.ts
+++ ui/webapp/src/app/modules/core/components/bd-data-component-cell/bd-data-component-cell.component.ts
@@ -1,11 +1,22 @@
-import { Component, ComponentRef, Input, OnDestroy, OnInit, Type, ViewContainerRef, inject } from '@angular/core';
+import {
+  Component,
+  ComponentRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  Type,
+  ViewContainerRef,
+  inject,
+} from '@angular/core';
 import { BdDataColumn } from 'src/app/models/data';
 
 @Component({
   selector: 'app-bd-data-component-cell',
   templateUrl: './bd-data-component-cell.component.html',
 })
-export class BdDataComponentCellComponent<T, X> implements OnInit, OnDestroy {
+export class BdDataComponentCellComponent<T, X> implements OnInit, OnChanges, OnDestroy {
   private readonly vc = inject(ViewContainerRef);
 
   @Input() record: T;
@@ -20,9 +31,13 @@ export class BdDataComponentCellComponent<T, X> implements OnInit, OnDestroy {
     this.componentRef.instance['column'] = this.column;
   }
 
-  ngOnDestroy(): void {
-    if (!this.componentRef) {
-      this.componentRef.destroy();
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.record && this.componentRef?.instance) {
+      this.componentRef.instance['record'] = changes.record.currentValue;
     }
+  }
+
+  ngOnDestroy(): void {
+    this.componentRef?.destroy();
   }
 }
