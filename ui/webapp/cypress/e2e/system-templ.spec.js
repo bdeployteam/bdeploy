72b45f6174f21b591efb33c82ead4b899f47655d
Dastan Yessekeyev <Dastan.Yessekeyev.extern@ssi-schaefer.com>
2024-08-20 06:48:38 +0500

    e2e check system variables
    Request: CT_BDEPLOY-135
    Change-Id: Ib41a078fb944927ba50b99f5e52a4d21ceb3bc1a

--- ui/webapp/cypress/e2e/system-templ.spec.js
+++ ui/webapp/cypress/e2e/system-templ.spec.js
@@ -91,4 +91,86 @@ describe('System Template Tests', () => {
       cy.contains('Chat Master').should('exist');
     });
   });
+
+  it('Check System Variables', () => {
+    cy.visit('/');
+    cy.enterGroup(groupName);
+
+    cy.pressMainNavButton('Systems');
+
+    cy.inMainNavContent(() => {
+      cy.get('tr:contains("Demo System")').should('exist').click();
+    });
+
+    cy.inMainNavFlyin('app-system-details', () => {
+      cy.get('button[data-cy^="System Variables"]').should('exist').click();
+    });
+
+    cy.inMainNavFlyin('app-system-variables', () => {
+      cy.contains('mat-expansion-panel', 'System Meta Information').within(() => {
+        cy.contains('mat-panel-description', '1/1 variables shown').should('exist');
+        cy.get('mat-panel-title').click();
+        cy.get('app-bd-form-input[name="system.variable.count_val"]')
+          .should('exist')
+          .within(() => {
+            cy.get('input[name="system.variable.count_val"]').should('have.value', '3');
+          });
+        cy.get('mat-panel-title').click();
+      });
+
+      cy.contains('mat-expansion-panel', 'System Variable Definitions').within(() => {
+        cy.contains('mat-panel-description', '2/2 variables shown').should('exist');
+        cy.get('mat-panel-title').click();
+        cy.get('app-bd-form-input[name="system.variable.numeric_val"]')
+          .should('exist')
+          .within(() => {
+            cy.get('input[name="system.variable.numeric_val"]').should('have.value', '1020');
+          });
+        cy.get('app-bd-form-input[name="system.variable.string_val"]')
+          .should('exist')
+          .within(() => {
+            cy.get('input[name="system.variable.string_val"]').should('have.value', 'defaultDefinedV1');
+          });
+        cy.get('mat-panel-title').click();
+      });
+
+      cy.contains('mat-expansion-panel', 'Ungrouped Variables').within(() => {
+        cy.contains('mat-panel-description', '1/1 variables shown').should('exist');
+        cy.get('mat-panel-title').click();
+        cy.get('app-bd-form-input[name="test.system.var_val"]')
+          .should('exist')
+          .within(() => {
+            cy.get('input[name="test.system.var_val"]').should('have.value', 'testValue');
+          });
+        cy.get('mat-panel-title').click();
+      });
+
+      cy.contains('mat-expansion-panel', 'Custom Variables').within(() => {
+        cy.contains('mat-panel-description', '0/0 variables shown').should('exist');
+        cy.get('mat-panel-title').click();
+        cy.get('button[data-cy="Add Custom Variable"]').click();
+      });
+
+      cy.contains('app-bd-notification-card', 'Add Variable').within(() => {
+        cy.fillFormInput('id', 'io.bdeploy.systemvar');
+        cy.fillFormInput('value_val', 'TheValue');
+        cy.fillFormInput('description', 'The Description');
+        cy.get('button[data-cy^=OK]').click();
+      });
+
+      cy.contains('mat-expansion-panel', 'Custom Variables').within(() => {
+        cy.contains('mat-panel-description', '1/1 variables shown').should('exist');
+      });
+
+      cy.get('button[data-cy^="Save"]').click();
+
+      cy.contains('app-bd-notification-card', 'Saving 4 instances').within(() => {
+        cy.contains(
+          'div',
+          'Affected 4 will be updated with the new system version. This needs to be installed and activated on all affected instances.',
+        );
+        cy.get('button[data-cy^=Yes]').click();
+      });
+    });
+  });
 });
