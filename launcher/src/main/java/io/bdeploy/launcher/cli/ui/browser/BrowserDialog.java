eaadd8d4def7d81dbc8b626ebd5854791918e408
Markus Duft <markus.duft@ssi-schaefer.com>
2024-09-10 07:48:14 +0200

    Increase default browser dialog width for linux
    Otherwise the buttons dont fit....
    
    Change-Id: I6b15263d3f9f7772dca00e379ea5788e56222607

--- launcher/src/main/java/io/bdeploy/launcher/cli/ui/browser/BrowserDialog.java
+++ launcher/src/main/java/io/bdeploy/launcher/cli/ui/browser/BrowserDialog.java
@@ -1,12 +1,6 @@
 package io.bdeploy.launcher.cli.ui.browser;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Desktop;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.KeyboardFocusManager;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -21,27 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
-import javax.swing.AbstractAction;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.InputMap;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
-import javax.swing.JProgressBar;
-import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.KeyStroke;
-import javax.swing.ListSelectionModel;
-import javax.swing.RowSorter;
-import javax.swing.SortOrder;
-import javax.swing.SwingConstants;
+import javax.swing.*;
 import javax.swing.SwingWorker.StateValue;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
@@ -135,7 +109,7 @@ public class BrowserDialog extends BaseDialog {
     private JProgressBar progressBar;
 
     public BrowserDialog(LauncherPathProvider lpp, Path userArea) {
-        super(1333);
+        super(1400);
 
         this.lpp = lpp;
         this.homeDir = lpp.get(SpecialDirectory.HOME);
