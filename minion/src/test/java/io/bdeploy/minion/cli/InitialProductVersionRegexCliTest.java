5ea039f3ffb8e667260076183ca47b32c88566a8
Daniel Kargl <daniel.kargl@ssi-schaefer.com>
2025-06-06 11:02:47 +0200

    Removed unused import from InitialProductVersionRegexCliTest
    Request: MO
    Change-Id: I1162bdeade1c98e9aa9eb046b65c3cb5b317032f

--- minion/src/test/java/io/bdeploy/minion/cli/InitialProductVersionRegexCliTest.java
+++ minion/src/test/java/io/bdeploy/minion/cli/InitialProductVersionRegexCliTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
