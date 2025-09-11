f722950f2271e46422878e3aa1a9d1d032e4bda0
Luiza Georza <luiza.georza.external@ssi-schaefer.com>
2025-09-11 15:25:24 +0300

    Renaming MultiNodeConnectionDescriptor
    To MultiNodeMasterFile since that describes better the information in
    the file.
    
    Request: MO
    Change-Id: Iaf274706ad2328a0d74d475623c26cd62d5c3be4

--- interfaces/src/main/java/io/bdeploy/interfaces/descriptor/node/MultiNodeMasterFile.java
+++ interfaces/src/main/java/io/bdeploy/interfaces/descriptor/node/MultiNodeMasterFile.java
@@ -5,11 +5,11 @@ import java.util.Objects;
 import io.bdeploy.common.security.RemoteService;
 
 /**
- * Describes the content of the {@literal Multi-Node connection} file.
+ * Describes the content of the {@literal Multi-Node master} file.
  * <p>
  * The file only contains immutable data which is required to connect to and register with the master.
  */
-public class MultiNodeConnectionDescriptor {
+public class MultiNodeMasterFile {
 
     /**
      * Remote master to connect to and register with
@@ -37,7 +37,7 @@ public class MultiNodeConnectionDescriptor {
         if (getClass() != obj.getClass()) {
             return false;
         }
-        MultiNodeConnectionDescriptor other = (MultiNodeConnectionDescriptor) obj;
+        MultiNodeMasterFile other = (MultiNodeMasterFile) obj;
         return Objects.equals(master, other.master) && Objects.equals(name, other.name);
     }
 }
