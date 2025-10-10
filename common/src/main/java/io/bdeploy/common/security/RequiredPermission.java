426c66a575e64c43005b2a67056b90d2898f8c47
Daniel Kargl <daniel.kargl@ssi-schaefer.com>
2025-10-10 10:53:47 +0200

    Fixed javadoc of RequiredPermission
    The annotation is for types, not classes. In fact, it is most commonly
    used for interfaces (besides methods), not for classes.
    
    Request: MO
    Change-Id: Ibb43903a918fcfc6061bc902d3ad0d4a0b26376c

--- common/src/main/java/io/bdeploy/common/security/RequiredPermission.java
+++ common/src/main/java/io/bdeploy/common/security/RequiredPermission.java
@@ -12,10 +12,10 @@ import io.bdeploy.common.security.ScopedPermission.Permission;
 /**
  * Specifies the permission required to access methods.
  * <p>
- * This annotation can be specified on a class or on method(s). Specifying it
- * at a class level means that it applies to all the methods in the class.
+ * This annotation can be specified on a type or on method(s). Specifying it
+ * at type level means that it applies to all the methods in the type.
  * Specifying it on a method means that it is applicable to that method only.
- * If applied at both the class and methods level, the method value overrides
+ * If applied at both the type and methods level, the method value overrides
  * the class value.
  * </p>
  */
