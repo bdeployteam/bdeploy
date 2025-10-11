5e7adb2cac0b47d35c3b33224702ac1829200735
Daniel Kargl <daniel.kargl@ssi-schaefer.com>
2025-10-11 14:10:40 +0200

    Added additional tests to AuthResourceTest
    Request: MO
    Change-Id: If43a2abde3edf9765772ec91fda16e6952d3f71d

--- minion/src/test/java/io/bdeploy/minion/ui/AuthResourceTest.java
+++ minion/src/test/java/io/bdeploy/minion/ui/AuthResourceTest.java
@@ -1,17 +1,25 @@
 package io.bdeploy.minion.ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.security.GeneralSecurityException;
+import java.util.Set;
+import java.util.SortedSet;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import io.bdeploy.api.remote.v1.dto.CredentialsApi;
 import io.bdeploy.common.security.ApiAccessToken;
+import io.bdeploy.common.security.ScopedPermission;
 import io.bdeploy.common.security.SecurityHelper;
+import io.bdeploy.interfaces.UserGroupInfo;
+import io.bdeploy.interfaces.UserInfo;
 import io.bdeploy.minion.TestMinion;
+import io.bdeploy.ui.api.AuthAdminResource;
 import io.bdeploy.ui.api.AuthResource;
 import jakarta.ws.rs.core.Response;
 
@@ -32,4 +40,47 @@ class AuthResourceTest {
         assertEquals("Test", decoded.getIssuedTo());
     }
 
+    @Test
+    void testCurrentUserDataUpdateLogic(AuthResource auth) {
+        UserInfo userInfo = auth.getCurrentUser();
+        UserInfo newUserInfo = new UserInfo(userInfo.name);
+
+        newUserInfo.fullName = "Ash Ketchum";
+        newUserInfo.email = "pikachu@thundershock.pkm";
+        auth.updateCurrentUser(newUserInfo);
+        userInfo = auth.getCurrentUser();
+        assertEquals(newUserInfo.fullName, userInfo.fullName);
+        assertEquals(newUserInfo.email, userInfo.email);
+
+        newUserInfo.fullName = "Gary Oak";
+        newUserInfo.email = "mewtu@mew.pkm";
+        auth.updateCurrentUser(newUserInfo);
+        userInfo = auth.getCurrentUser();
+        assertEquals(newUserInfo.fullName, userInfo.fullName);
+        assertEquals(newUserInfo.email, userInfo.email);
+    }
+
+    @Test
+    void testCurrentUserGroupLogic(AuthResource auth) {
+        UserGroupInfo userGroupInfo = new UserGroupInfo();
+        userGroupInfo.name = "TestGroup";
+        userGroupInfo.description = "Cool Test Group";
+        userGroupInfo.permissions = Set.of(new ScopedPermission(ScopedPermission.Permission.READ));
+
+        AuthAdminResource authAdmin = auth.getAdmin();
+
+        authAdmin.createUserGroup(userGroupInfo);
+
+        SortedSet<UserGroupInfo> allUserGroups = authAdmin.getAllUserGroups();
+        userGroupInfo = allUserGroups.stream().filter(g -> g.name.equals("TestGroup")).findAny().get();
+        String userGroupId = userGroupInfo.id;
+
+        String currentUserName = auth.getCurrentUser().name;
+
+        assertFalse(auth.getCurrentUserProfile().userGroups.contains(userGroupInfo));
+        authAdmin.addUserToGroup(userGroupId, currentUserName);
+        assertTrue(auth.getCurrentUserProfile().userGroups.contains(userGroupInfo));
+        authAdmin.removeUserFromGroup(userGroupId, currentUserName);
+        assertFalse(auth.getCurrentUserProfile().userGroups.contains(userGroupInfo));
+    }
 }
